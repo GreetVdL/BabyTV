b850b7af9c8d48b8f4475631e24419ac5f53dbd6
Greet <vanderlindengreet@gmail.com>
2021-06-03 23:53:56 +0200

    code finished

--- src/js/index.js
+++ src/js/index.js
@@ -2,40 +2,53 @@ import "../css/style.scss";
 import { Circle, randomRange } from "./components/circle.js";
 
 const holder = document.querySelector("#container");
+const button = document.querySelector("#button");
 
 const amountCircles = 100;
 
 const promises = [];
 const allCircles = [];
 
-// Draw all circles
-for (let i = 1; i <= amountCircles; i++) {
-  // Generate random values
-  const diameter = randomRange(50, 250);
-  const x = Math.round((document.body.offsetWidth - diameter) * Math.random());
-  const y = Math.round((document.body.offsetHeight - diameter) * Math.random());
+button.addEventListener("click", (event) => {
+  // Button disappears
+  button.style.display = "none";
 
-  // Draw circle
-  promises.push(
-    new Promise((resolve) => {
-      setTimeout(() => {
-        allCircles.push(new Circle(diameter, x, y, holder));
-        resolve();
-      }, 200 * i);
-    })
-  );
-}
+  // Draw all circles
+  for (let i = 1; i <= amountCircles; i++) {
+    // Generate random values
+    const diameter = randomRange(50, 250);
+    const x = Math.round(
+      (document.body.offsetWidth - diameter) * Math.random()
+    );
+    const y = Math.round(
+      (document.body.offsetHeight - diameter) * Math.random()
+    );
 
-// When all circles are drawn, they turn pink and disappear
-async function pinkify() {
-  await Promise.all(promises);
-  allCircles.forEach((circle) => {
-    circle.htmlRef.style.backgroundColor = "#F8C6E7";
-    circle.htmlRef.style.border = "3px solid #F8C6E7";
-  });
-  setTimeout(() => {
-    allCircles.forEach((circle) => circle.clear());
-  }, 2000);
-}
+    // Draw circle
+    promises.push(
+      new Promise((resolve) => {
+        setTimeout(() => {
+          allCircles.push(new Circle(diameter, x, y, holder));
+          resolve();
+        }, 200 * i);
+      })
+    );
+  }
 
-pinkify();
+  // When all circles are drawn, they turn pink and disappear and the "play" button appears again
+  async function finish() {
+    await Promise.all(promises);
+    allCircles.forEach((circle) => {
+      circle.htmlRef.style.backgroundColor = "#F8C6E7";
+      circle.htmlRef.style.border = "3px solid #F8C6E7";
+    });
+    setTimeout(() => {
+      allCircles.forEach((circle) => circle.clear());
+    }, 2000);
+    setTimeout(() => {
+      button.style.display = "block";
+    }, 2000);
+  }
+
+  finish();
+});
