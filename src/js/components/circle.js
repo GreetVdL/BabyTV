4ebe90444aa95900c4b467aecf4fcaf8263423f4
Greet <vanderlindengreet@gmail.com>
2021-06-03 21:00:41 +0200

    halfway there

--- src/js/components/circle.js
+++ src/js/components/circle.js
@@ -1,1 +1,50 @@
 import randomColor from "randomcolor";
+
+// helper functions
+function randomRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+// circle class
+export default class Circle {
+  constructor() {
+    this.width = randomRange(50, 250);
+    this.x = Math.round(
+      (document.body.offsetWidth - this.width) * Math.random()
+    );
+    this.y = Math.round(
+      (document.body.offsetHeight - this.width) * Math.random()
+    );
+    this.holder = document.querySelector("#container");
+    this.htmlRef = this.generateHTML();
+    this.applyStyling();
+  }
+  generateHTML() {
+    this.holder.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="circle"></div>
+      `
+    );
+    return this.holder.querySelector(".circle:last-child");
+  }
+  applyStyling() {
+    const styles = {
+      width: this.width + "px",
+      height: this.width + "px",
+      position: "absolute",
+      top: this.y + "px",
+      left: this.x + "px",
+      borderRadius: "50%",
+      animationDuration: randomRange(5, 20) + "s",
+      backgroundColor: randomColor({
+        luminosity: "light",
+        format: "hsla", // e.g. 'hsla(27, 88.99%, 81.83%, 0.6450211517512798)'
+      }),
+      border: `3px solid ${randomColor({
+        luminosity: "light",
+      })}`,
+    };
+    Object.assign(this.htmlRef.style, styles);
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,33 @@
 import "../css/style.scss";
+import Circle from "./components/circle.js";
+
+const amountCircles = 10;
+
+let count = 0;
+
+// function circleDraw() {
+//   new Circle();
+//   count++;
+//   console.log(count);
+// }
+
+// const interval = setInterval(circleDraw, 200);
+
+// if (count === amountCircles) {
+//   clearInterval(interval);
+// }
+
+function wait() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), 1000);
+  });
+}
+
+async function drawCircle() {
+  new Circle();
+  await wait();
+}
+
+for (let i = 0; i < amountCircles; i++) {
+  drawCircle();
+}
